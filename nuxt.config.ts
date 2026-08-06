503736871f3243d38ed6a270111750399f32691b
Matt Shaw <imhullu15@gmail.com>
2026-08-06 11:30:29 +0100

    Updated title and favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@pinia/nuxt']
+
+  devtools: {
+    enabled: true,
+  },
+
+  modules: ['@pinia/nuxt'],
+
+  app: {
+    head: {
+      title: 'Flag Guessing Game',
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/png',
+          href: '/favicon.png',
+        },
+      ],
+    },
+  },
 })
